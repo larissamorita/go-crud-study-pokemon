1a7fb686163b4c2bda1a178c1917a9518475dd78
larissamorita <morita.larissa@gmail.com>
2024-02-24 15:38:49 -0300

    Exercicio pkm get

--- resource/pokemon.go
+++ resource/pokemon.go
@@ -1,1 +1,13 @@
 package resource
+
+type ResponsePKM struct {
+	PkmEntries []DataPkm `json:"pokemon_entries"`
+}
+type DataPKM struct {
+	entryNumber int           `json:"entry_number"`
+	pkmSpecies  []DataSpecies `json:"pokemon_species"`
+}
+type DataSpecies struct {
+	pkmName string `json:"name"`
+	pkmURL  string `json:"url"`
+}
--- service/pokemon.go
+++ service/pokemon.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/larissamorita/go-crud-study-pokemon/resource"
+	"io"
+	"log"
+	"net/http"
+)
+
+func searchpkmR() {
+
+	resp, err := http.Get("https://pokeapi.co/api/v2/pokedex/kanto")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bytesBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	responseStruct2 := resource.ResponsePKM{}
+
+	err = json.Unmarshal(bytesBody, &responseStruct2)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Print(searchpkmR)
+}
